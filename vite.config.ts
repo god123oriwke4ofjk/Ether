ef6e2027192633c48c5be78daaeb562ef61c38d3
dagiikg9134 <163017856+god123oriwke4ofjk@users.noreply.github.com>
2025-05-27 21:13:23 +0300

    add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import wallbashHmrPlugin from "./src/vite-plugin-wallbash-hmr";
+import symlinkImagePlugin from "./src/vite-plugin-symlink-image";
 
 export default defineConfig({
-  plugins: [wallbashHmrPlugin()],
+  plugins: [
+    wallbashHmrPlugin(),
+    symlinkImagePlugin(),
+  ],
   server: {
     hmr: {
       overlay: true,
